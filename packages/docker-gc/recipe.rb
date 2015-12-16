3a9afab51faa0371afba6cbe9497c2285dd0f10d
Simon Rozet <me@simonrozet.com>
2015-12-16 15:52:40 +0000

    use fpm-cookery

---
+++ packages/docker-gc/recipe.rb
@@ -0,0 +1,16 @@
+class DockerGC < FPM::Cookery::Recipe
+  name "docker-gc"
+  version "0.1.0"
+  homepage "https://github.com/spotify/docker-gc"
+  source "https://github.com/spotify/docker-gc",
+    :with => "git",
+    :sha  => "e194bc0fec47e55e03d94fb2e5b4260748f2cfae"
+
+  def build
+  end
+
+  def install
+    bin.install("docker-gc")
+    doc("docker-gc").install("README.md")
+  end
+end
