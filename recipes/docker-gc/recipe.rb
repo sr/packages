e5f196d37a5b9a723f06e9d9e769aeb140133290
Simon Rozet <me@simonrozet.com>
2015-12-16 16:09:04 +0000

    set maintainer for docker-gc

--- recipes/docker-gc/recipe.rb
+++ recipes/docker-gc/recipe.rb
@@ -2,6 +2,7 @@ class DockerGC < FPM::Cookery::Recipe
   name "docker-gc"
   version "0.1.0"
   homepage "https://github.com/spotify/docker-gc"
+  maintainer "Simon Rozet <me@simonrozet.com>"
   source "https://github.com/spotify/docker-gc",
     :with => "git",
     :sha  => "e194bc0fec47e55e03d94fb2e5b4260748f2cfae"
